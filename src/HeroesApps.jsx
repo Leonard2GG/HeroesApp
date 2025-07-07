e109608729d9a92b10a7cb2438547bc68242e76a
Leonardo González <kiri05062001@gmail.com>
2025-07-07 12:13:13 -0400

    first commit

---
+++ src/HeroesApps.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeroesApps() {
+    return (
+        <div>
+            <h1>Heroes App</h1>
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import HeroesApps from './HeroesApps'
+
+createRoot(document.getElementById('root')).render(
+  <HeroesApps />
+)
